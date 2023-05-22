52710f68068cc2f8f86368d5d7dba31c997d902c
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-05-22 10:52:54 +0200

    add new libraries

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/style/index.css"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Home />
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home />}></Route>
+            </Routes>
+        </Router>
     </React.StrictMode>
 );
