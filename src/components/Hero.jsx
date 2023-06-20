a3ca73aa159de249963cbfd6a76d37b4c2b195c8
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-06-20 13:54:08 +0200

    some modification

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,28 +3,27 @@ import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 import { IconContext } from "react-icons";
 import { MostResearchedBreeds } from "./MostResearchedBreeds";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+//import { useFetch } from "../hooks/useFetch";
+//import { Link } from "react-router-dom";
 
 export const Hero = () => {
-    const [breeds, setBreed] = useState([]);
-
-    const fetchBreedData = () => {
-        fetch(
-            `https://api.thecatapi.com/v1/breeds?api_key=${process.env.REACT_APP_api_key}`
-        )
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                setBreed(data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
+    const [breeds, setBreed] = useState(null);
 
     useEffect(() => {
-        fetchBreedData();
+        const getData = async () => {
+            try {
+                const res = await fetch(
+                    `https://api.thecatapi.com/v1/breeds?api_key=${process.env.REACT_APP_api_key}`
+                );
+                let data = await res.json();
+                setBreed(data);
+            } catch (error) {
+                setBreed(null);
+                console.log(error);
+            }
+        };
+
+        getData();
     }, []);
 
     return (
@@ -38,37 +37,24 @@ export const Hero = () => {
                 <p className="text-white text-xl lg:text-2xl w-1/2 lg:w-1/5 mb-5 lg:mb-10">
                     Get to know more about your cat breed
                 </p>
-                <form className="lg:mb-12">
-                    <div className="md:hidden">
-                        <select className="rounded-full p-2 px-4 w-1/2 lg:w-1/4 text-black text-sm">
-                            <option value="" disabled selected>
+
+                <div>
+                    <form action="">
+                        <select className="rounded-full p-2 px-4 w-1/2 lg:w-1/4 text-black">
+                            <option value="Select a breed" disabled selected>
                                 Select a breed
                             </option>
-                            {breeds.map((breed) => (
-                                <Link to={`/breed/${breed.id}`}>
-                                    <option value={breed.id}>
+                            {breeds &&
+                                breeds.map((breed) => (
+                                    <option value={breed.id} key={breed.id}>
                                         {breed.name}
                                     </option>
-                                </Link>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="hidden md:block">
-                        <form action="">
-                            <select className="rounded-full p-2 px-4 w-1/2 lg:w-1/4 text-black">
-                                <option value="" disabled selected>
-                                    Select a breed
-                                </option>
-                                {breeds.map((breed) => (
-                                    <option value={breed.id}>
-                                        <Link to={`/breed/${breed.id}`}>{breed.name}</Link>
-                                    </option>
                                 ))}
-                            </select>
-                        </form>
-                    </div>
-                </form>
+                        </select>
+                    </form>
+                </div>
             </div>
+
             <div className="bg-slate-300 py-8 lg:py-16 px-12 rounded-b-3xl">
                 <p className="text-main-color lg:text-xl mb-5">
                     Most Researched Breeds
@@ -78,12 +64,12 @@ export const Hero = () => {
                         66+ Breeds For You to discover
                     </p>
                     <div className="flex gap-3 cursor-pointer">
-                        <Link
+                        <p
                             className="self-center text-secondary-color uppercase"
                             to="/most-popular-breed"
                         >
                             see more
-                        </Link>
+                        </p>
                         <IconContext.Provider
                             value={{
                                 style: { color: "rgba(41, 21, 7, 0.6)" },
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./assets/style/index.css"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { HomeDetails } from "./pages/HomeDetails";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,6 +11,7 @@ root.render(
         <Router>
             <Routes>
                 <Route exact path="/" element={<Home />}></Route>
+                <Route path="/breed" element={<HomeDetails />}></Route>
             </Routes>
         </Router>
     </React.StrictMode>
