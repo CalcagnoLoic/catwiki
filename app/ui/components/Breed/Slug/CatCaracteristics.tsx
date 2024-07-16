61771d323fefd533765223e13cf58b0b9bf8e5e8
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-07-16 14:21:00 +0200

    fixing mobile view for CatCaracteristics

--- app/ui/components/Breed/Slug/CatCaracteristics.tsx
+++ app/ui/components/Breed/Slug/CatCaracteristics.tsx
@@ -1,6 +1,7 @@
 import { Cat } from "@/app/lib/definitions";
 import { catCaracteristics } from "@/app/lib/data";
-import { Icons } from "@/app/ui/icons";
+
+import Paragraph from "../../Paragraph";
 
 const CatCaracteristics = ({ breed }: { breed: Cat }) => {
   const stars = Array(5).fill(0);
@@ -9,18 +10,33 @@ const CatCaracteristics = ({ breed }: { breed: Cat }) => {
     <>
       {catCaracteristics.map((caracteristic) => (
         <div className="mt-8" key={caracteristic.name}>
-          <div className="flex">
+          <div className="flex flex-col md:flex-row">
             <strong className="mr-2">{caracteristic.name}: </strong>
-            {caracteristic.rating === false && breed[caracteristic.data]}
-            {caracteristic.rating === true &&
-              stars.map((_, index) => (
-                <div key={index} className="self-center md:ml-5">
-                  <div
-                    className={`h-[12px] w-[60px] rounded-2xl ${breed[caracteristic.data] > index ? "bg-cioccolato" : "bg-alto"}`}
-                  ></div>
-                </div>
-              ))}
-            {caracteristic.name === "Life Span" && " years"}
+
+            {caracteristic.rating === false && (
+              <Paragraph
+                kind="p"
+                css="mt-3 md:mt-0"
+                content={
+                  <>
+                    {breed[caracteristic.data]}
+                    {caracteristic.name === "Life Span" && " years"}
+                  </>
+                }
+              />
+            )}
+
+            {caracteristic.rating === true && (
+              <div className="mt-3 flex gap-3 md:mt-0 md:gap-0">
+                {stars.map((_, index) => (
+                  <div key={index} className="self-center md:ml-5">
+                    <div
+                      className={`h-[12px] w-[35px] rounded-2xl md:w-[60px] ${breed[caracteristic.data] > index ? "bg-cioccolato" : "bg-alto"}`}
+                    ></div>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       ))}
